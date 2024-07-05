392100c20dac49ca8b9a4ffffb4b82ee26abbd3a
EduardoWodtke <eduardorwodtkeifc@gmail.com>
2024-07-05 15:00:30 -0300

    Atualização de Null True

--- core/models/marca.py
+++ core/models/marca.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Marca(models.Model):
     nome = models.CharField(max_length=100)
-    nacionalidade = models.CharField(max_length=100)
+    nacionalidade = models.CharField(max_length=50, blank=True, null=True)    
 
     def __str__(self):
         return f"{self.nome} {self.nacionalidade} ({self.id})"
